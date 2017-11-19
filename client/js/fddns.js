cc714254921050e41c15eb96ef1a2550f06f8b67
Fares ABADOU <fares.abadou@gmail.com>
2017-11-19 17:40:53 +0100

    ajax call

--- client/js/fddns.js
+++ client/js/fddns.js
@@ -1,29 +1,40 @@
 
 var fddns = {
-  base : 'http://example.com/server/' ,
+  base : 'http://localhost/server/' ,
   name : '' ,
   adress : '' ,
   phone : '' ,
   saveTo : 'save.php' ,
-  cb : function(e) ,
+
   init : function(param){
 if (param.name) this.name = param.name ;
+if (param.client) this.client = param.client ;
 if (param.adress) this.adress = param.adress ;
 if (param.phone) this.phone = param.phone ;
 if (param.saveTo) this.saveTo = param.saveTo ;
-if (param.cb) this.cb = param.cb ;
-ffdns.run();
+if (param.base) this.base = param.base ;
+if (param.callback) this.cb = param.callback ;
+fddns.run();
 } ,
 run : function(){
 
-$(function(){
   $.ajax({
-    url: fddns.base,
-    dataType: "jsonp",
-    jsonpCallback: "logResults"
-  });
+      type: 'POST',
+      url: fddns.base+'creat.php',
+      crossDomain: true,
+      data: {'name':fddns.name ,'client':fddns.client ,'adress':fddns.adress  , 'phone':fddns.phone },
+      dataType: 'text',
+      success: function(responseData, textStatus, jqXHR) {
+           // console.log(responseData);
+var res = JSON.parse(responseData);
+if (fddns.cb) fddns.cb(res);
 
-});
+      },
+      error: function (responseData, textStatus, errorThrown) {
+
+        console.log('error 5');
+      }
+  });
 
 
 
--- server/creat.php
+++ server/creat.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('XDomainRequestAllowed: 1');
 $res = array();
 $res['status'] = 'error';
 $name = $adress = $phone = $client = '';
@@ -7,12 +9,13 @@ if ($name != '' && $adress != '' && ($client == 'em' || $client = 'menumax' )) {
   $ddns = new PDO('sqlite:data/main.db');
 $tocken = md5(uniqid(rand(), true));
 $DATETIME = date('Y-m-d H:i:s');
-$ddns->exec("INSERT INTO $client (NAME,ADRESS,PHONE,TOCKEN,DATETIME) VALUES ('$name','$adress','$tel','$token','$DATETIME') ;");
+$ddns->exec("INSERT INTO $client (NAME,ADRESS,PHONE,TOCKEN,DATETIME) VALUES ('$name','$adress','$phone','$tocken','$DATETIME') ;");
 $res['status'] = 'ok';
 $res['token'] = $tocken;
 }
+$res = json_encode($res);
 
-
-exit( $_GET['callback'] . ' (' . stripslashes($res) . ');' );
+// exit( $_GET['callback'] . ' (' . stripslashes($res) . ');' );
+exit( $res   );
 
  ?>
