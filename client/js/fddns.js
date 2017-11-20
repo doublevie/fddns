1d2659ba0466c0618a4106fcc4d30ed37129be6d
Fares ABADOU <fares.abadou@gmail.com>
2017-11-20 15:02:02 +0100

    a -set -get

--- client/js/fddns.js
+++ client/js/fddns.js
@@ -4,13 +4,30 @@ var fddns = {
   name : '' ,
   adress : '' ,
   phone : '' ,
+  client : 'em' ,
   saveTo : 'save.php' ,
+check : function(par){
+  console.log('checking');
+  $.ajax({
+      type: 'GET',
+      url: 'fddns/check.php',
+      dataType: 'text',
+      success: function(responseData, textStatus, jqXHR) {
+ if (par.callback) {
+   par.callback(responseData);
+ }
+      },
+      error: function (responseData, textStatus, errorThrown) {
 
+        console.log('error 5');
+      }
+  });
+},
   init : function(param){
 if (param.name) this.name = param.name ;
 if (param.client) this.client = param.client ;
 if (param.adress) this.adress = param.adress ;
-if (param.phone) this.phone = param.phone ;
+
 if (param.saveTo) this.saveTo = param.saveTo ;
 if (param.base) this.base = param.base ;
 if (param.callback) this.cb = param.callback ;
@@ -22,13 +39,14 @@ run : function(){
       type: 'POST',
       url: fddns.base+'creat.php',
       crossDomain: true,
-      data: {'name':fddns.name ,'client':fddns.client ,'adress':fddns.adress  , 'phone':fddns.phone },
+      data: {'name':fddns.name ,'client':fddns.client ,'adress':fddns.adress  },
       dataType: 'text',
       success: function(responseData, textStatus, jqXHR) {
-           // console.log(responseData);
+
 var res = JSON.parse(responseData);
 if (fddns.cb) fddns.cb(res);
 
+if (res.token !== undefined) fddns.save(res.token);
       },
       error: function (responseData, textStatus, errorThrown) {
 
@@ -39,5 +57,9 @@ if (fddns.cb) fddns.cb(res);
 
 
 
+} ,
+save : funcion() {
+  
 }
+
 }
--- server/creat.php
+++ server/creat.php
@@ -3,19 +3,19 @@ header('Access-Control-Allow-Origin: *');
 header('XDomainRequestAllowed: 1');
 $res = array();
 $res['status'] = 'error';
-$name = $adress = $phone = $client = '';
+$name = $adress =  $client = '';
 extract($_POST);
-if ($name != '' && $adress != '' && ($client == 'em' || $client = 'menumax' )) {
+$res['status'] = $client;
+if ($name != '' && $adress != '' && ($client == 'em' || $client == 'menumax' )) {
   $ddns = new PDO('sqlite:data/main.db');
 $tocken = md5(uniqid(rand(), true));
 $DATETIME = date('Y-m-d H:i:s');
-$ddns->exec("INSERT INTO $client (NAME,ADRESS,PHONE,TOCKEN,DATETIME) VALUES ('$name','$adress','$phone','$tocken','$DATETIME') ;");
 $res['status'] = 'ok';
+$ddns->exec("INSERT INTO $client (NAME,ADRESS,TOKEN,DATETIME) VALUES ('$name','$adress' ,'$tocken','$DATETIME') ;")  OR $res['status'] = $ddns->errorInfo();
 $res['token'] = $tocken;
 }
 $res = json_encode($res);
 
-// exit( $_GET['callback'] . ' (' . stripslashes($res) . ');' );
-exit( $res   );
+exit($res);
 
  ?>
